d6a0d3138ee4f453b33748df64ef02256e873f75
linhmt13113 <linh.pp269@gmail.com>
2023-04-25 10:50:58 +0700

    Init Project

---
+++ Project-Net/Application/Interfaces/IWsGameServer.cs
@@ -0,0 +1,8 @@
+namespace Project_Net.Application.Interfaces;
+
+public interface IWsGameServer
+{
+    void StartServer();
+    void StopServer();
+    void RestartServer();
+}
---
+++ Project-Net/Main.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Net;
+using Project_Net.Application.Handlers;
+
+var wsServer = new WsGameServer(IPAddress.Any, 8080);
+wsServer.StartServer();
+for (;;)
+{
+    var type = Console.ReadLine();
+    if (!string.IsNullOrEmpty(type = "restart"))
+    {
+        wsServer.RestartServer();
+    }
+
+    if (!string.IsNullOrEmpty(type = "shutdown"))
+    {
+        wsServer.StopServer();
+        break;
+    }
+}
